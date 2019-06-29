eb6dd02bd9bd53b165ed6579e1d618aefe98a396
YutoOshima <oshima1998@gmail.com>
2019-06-30 04:21:18 +0900

    Finish User Create

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'homes/index'
+  resources :users, only: [:new, :create, :update, :destory]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
